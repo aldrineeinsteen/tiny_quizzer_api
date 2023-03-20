5f613bf159dfe497b8a0d2b553eaa9afa4658d85
Aldrine Einsteen <aldrine.einsteen@hotmail.com>
2023-03-20 12:16:48 +0000

    Spring Init commit

---
+++ src/main/java/com/tinyquizzer/api/tinyquizzerapi/TinyQuizzerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tinyquizzer.api.tinyquizzerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyQuizzerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyQuizzerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinyquizzer/api/tinyquizzerapi/TinyQuizzerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinyquizzer.api.tinyquizzerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyQuizzerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
